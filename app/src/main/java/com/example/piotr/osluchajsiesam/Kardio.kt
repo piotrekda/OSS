137d2c1e07e6417a7497fb40a7095b7be7adabae
piotr <piotrekda@gmail.com>
2018-03-18 14:18:01 +0100

    przed edycją tekstu

--- app/src/main/java/com/example/piotr/osluchajsiesam/Kardio.kt
+++ app/src/main/java/com/example/piotr/osluchajsiesam/Kardio.kt
@@ -1,12 +1,25 @@
 package com.example.piotr.osluchajsiesam
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class Kardio : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_kardio)
+    }
+
+    fun toPrawidloweTonySerca(view: View) {
+        val intent = Intent(applicationContext, PrawidloweTonySerca::class.java)
+        startActivity(intent)
+
+    }
+    fun toJakOsluchiwac(view: View) {
+        val intent = Intent(applicationContext, JakOsluchiwac::class.java)
+        startActivity(intent)
+
     }
 }
---
+++ app/src/main/java/com/example/piotr/osluchajsiesam/PrawidloweTonySerca.kt
@@ -0,0 +1,12 @@
+package com.example.piotr.osluchajsiesam
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class PrawidloweTonySerca : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_prawidlowe_tony_serca)
+    }
+}
